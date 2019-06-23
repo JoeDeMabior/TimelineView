7202defd69d47b66e7bef008bfd0a58b7e954176
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-23 13:02:59 +0300

    Create the IndicatorAdapter and add it to the TimeLineView

--- app/src/main/java/com/joe/timelineview/MainActivity.kt
+++ app/src/main/java/com/joe/timelineview/MainActivity.kt
@@ -2,7 +2,12 @@ package com.joe.timelineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import me.jerryhanks.timelineview.IndicatorAdapter
+import me.jerryhanks.timelineview.interfaces.TimeLineViewCallback
 import me.jerryhanks.timelineview.model.Status
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timelines = mutableListOf<MyTimeline>().apply {
+        val timeLines = mutableListOf<MyTimeline>().apply {
             add(MyTimeline(Status.COMPLETED, getString(R.string.s_title_1), getString(R.string.s_content_1)))
             add(MyTimeline(Status.COMPLETED, getString(R.string.s_title_2), getString(R.string.s_content_2)))
             add(MyTimeline(Status.COMPLETED, getString(R.string.s_title_3), getString(R.string.s_content_3)))
@@ -27,5 +32,35 @@ class MainActivity : AppCompatActivity() {
             add(MyTimeline(Status.COMPLETED, getString(R.string.s_title_11), getString(R.string.s_content_11)))
             add(MyTimeline(Status.COMPLETED, getString(R.string.s_title_12), getString(R.string.s_content_12)))
         }
+
+        adapter = IndicatorAdapter(mutableListOf(), this, object : TimeLineViewCallback<MyTimeline> {
+            override fun onBindView(model: MyTimeline, container: FrameLayout, position: Int): View {
+                val view = layoutInflater.inflate(R.layout.sample_time_line, container, false)
+                view.findViewById<TextView>(R.id.tv_title).text = model.title
+                view.findViewById<TextView>(R.id.tv_content).text = model.content
+                return view
+            }
+        })
+
+        timeline_view.setIndicatorAdapter(adapter)
+        adapter.swapItems(timeLines)
+
+        caption.text = getString(R.string.delivery_status)
+
+        btn_add_to_top.setOnClickListener { addToTop() }
+        btn_add_to_bottom.setOnClickListener { addToBottom() }
     }
+
+    private fun addToTop() {
+        val timeline = MyTimeline(Status.ATTENTION, "New Top One", "Hehehehe")
+        adapter.addItems(timeline, index = 2)
+        timeline_view.scrollToTop()
+    }
+
+    private fun addToBottom() {
+        val timeline = MyTimeline(Status.ATTENTION, "New Bottom One", "Hahahahaha")
+        adapter.addItems(timeline)
+        timeline_view.scrollToBottom()
+    }
+
 }
